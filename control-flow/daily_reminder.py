b1eb137672a2392a60665662cd6669698efe297e
mmesbaah <mohannad.mesbaa7@gmail.com>
2025-03-23 00:07:10 +0200

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -12,7 +12,7 @@ match priority:
         reminder = f" '{task}' has an unknown priority level"
         
 if time_bound == "yes":
-    reminder += " that requires immediate attention today!"
+    reminder += " that requires immediate attention today! "
 else:
     reminder += " Consider completing it when you have free time. "
 print(reminder)
