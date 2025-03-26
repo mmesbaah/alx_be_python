cf54c231826aab2512657157f34b68b50e0e2b17
mmesbaah <mohannad.mesbaa7@gmail.com>
2025-03-27 01:34:58 +0200

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,6 +1,6 @@
 class Book:
     def __init__(self, title, author, year):
-        self.titel = title
+        self.title = title
         self.author = author
         self.year = year
     def __str__(self):
