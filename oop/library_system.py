7943f842dabd951583eee7ecf0bdd0970c636262
mmesbaah <mohannad.mesbaa7@gmail.com>
2025-03-27 02:05:55 +0200

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,7 +1,7 @@
 class Book:
     def __init__(self, title, author):
         self.title = title
-        self.autho = author
+        self.author = author
     def __str__(self):
         return f"Book: {self.title} by {self.author}"
 class EBook(Book):
